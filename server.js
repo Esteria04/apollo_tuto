bbad563e57ee62b611a88f5f49c5e7b60425af08
Esteria04 <dev.esteria04@gmail.com>
2023-05-20 23:08:21 +0900

    .

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const typeDef = gql`
+    type Query {
+        type Author {
+            name: String,
+            articles: [Article],
+            comments: [Comment]
+        }
+        type Article {
+            author: Author,                                       
+            publish-date: String,
+            modify-date: String,
+            title: String,
+            content: String,
+            comments: [Comment]
+        }
+        type Comment {
+            author: Author,
+            article: Article,
+            content: String
+        }
+    }
+`;
+
 const server = new ApolloServer({});
-server.listen().then(({url})=>{console.log(`Running on the ${url}`)})
+server.listen().then(({url})=>{
+    console.log(`Running on the ${url}`);
+});
