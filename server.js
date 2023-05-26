cc0a552bd90738f3397e5b0137c3cdc7cf1d516c
Esteria04 <huijunyi0212@naver.com>
2023-05-26 22:36:15 +0900

    .

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+    {
+        id:"1",
+        text:"hello"
+    },
+    {
+        id:"2",
+        text:"hello1"
+    },
+    {
+        id:"3",
+        text:"hello2"
+    },
+    {
+        id:"4",
+        text:"hello3"
+    },
+]
+
 const typeDefs = gql`
     type User {
         id: ID!
@@ -13,6 +32,7 @@ const typeDefs = gql`
     type Query {
         allTweets: [Tweet!]!
         tweet(id: ID): Tweet
+        ping: String!
     }
     type Mutation {
         postTweet(text: String!, userId: ID!): Tweet!
@@ -20,7 +40,22 @@ const typeDefs = gql`
     }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+    Query: {
+        tweet() {
+            console.log("I'm called");
+            return null;
+        },
+        ping() {
+            return "pong";
+        },
+        allTweets() {
+            return tweets;
+        }
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({url})=>{
     console.log(`Running on ${url}`);
-});
+});  
